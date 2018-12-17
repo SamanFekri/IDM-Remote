ec03b9cd3795c649e95560a60d471ab65df612c7
skings <samanf74@gmail.com>
2018-12-17 22:08:06 +0330

    finish the api section

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 'use strict';
 
 const Hapi = require('hapi');
-const config = require('./config');
+const Config = require('./config');
 const NodeRSA = require('node-rsa');
+const Joi = require('joi');
+const cmd = require('node-cmd');
 
-
-const key = new NodeRSA(config.priKey);
-key.importKey(config.pubKey, 'public');
+const key = new NodeRSA(Config.priKey);
+key.importKey(Config.pubKey, 'public');
 
 // Create a server with a host and port
 const server = Hapi.server({
-  host: config.ip,
-  port: config.port
+  host: Config.ip,
+  port: Config.port
 });
 
 // Add the route
@@ -21,6 +22,48 @@ server.route({
   handler: function (request, h) {
 
     return 'hello world';
+  }
+});
+
+server.route({
+  method: 'POST',
+  path: '/api/start',
+  config: {
+    validate: {
+      payload: {
+        secret: Joi.string().required(),
+        list: Joi.string().required(),
+        user: Joi.string().required()
+      }
+    }
+  },
+  handler: function (request, h) {
+    if (request.payload.user in Config.users) {
+      let decrypted = key.decrypt(request.payload.secret, 'utf8');
+      console.log(decrypted);
+      if (Config.users[request.payload.user].secret === decrypted) {
+        let list = JSON.parse(request.payload.list);
+        if (list.length > 0) {
+          for (let i in list) {
+            let item = list[i];
+            let line = ``;
+            if ('name' in item) {
+              line = `idman /n /a /d "${item.link}" /f ${item.name}`;
+            } else {
+              line = `idman /n /a /d "${item.link}"`;
+            }
+            cmd.get(line, (err, data, stderr) => {
+              console.log(`Data: ${data}`);
+              console.log(`Err: ${err}`);
+              console.log(`StdErr: ${stderr}`);
+            });
+          }
+          cmd.run('idman /s');
+          return {code: 200};
+        }
+      }
+    }
+    return request.payload;
   }
 });
 
